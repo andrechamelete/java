c849a7f67bb9a8c762446b60a66ec2deb8477417
andrechamelete <chamelete@gmail.com>
2024-03-01 13:36:53 -0300

    modificação em Paciente.java e Main.java

--- IMC/Main.java
+++ IMC/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[]args) {
         Paciente p1 = new Paciente(70, 1.78);
 
-        p1.calcularIMC(p1);
-        System.out.println(p1.imc);
+        p1.calcularIMC();
+        System.out.println(p1.calcularIMC());
 
-        p1.diagnostico(p1);
+        p1.diagnostico();
     }
 }
--- IMC/Paciente.java
+++ IMC/Paciente.java
@@ -1,37 +1,33 @@
 public class Paciente {
 
-    //campos de estado do paciente
     public double peso;
     public double altura;
-    public double imc;
 
-    //contructor
     public Paciente(double peso, double altura){
         this.peso = peso;
         this.altura = altura;
     }
 
-    //método para retornar IMC do paciente
-    public double calcularIMC(Paciente p) {
-        p.imc = p.peso / (p.altura * p.altura);
-        return p.imc;
+    public double calcularIMC() {
+        double imc = 0;
+        imc = peso / (altura * altura);
+        return imc;
     }
 
-    //método para retornar o diagnoóstico em relação ao IMC do paciente
-    public void diagnostico(Paciente p) {
-        if (p.imc < 16)
+    public void diagnostico() {
+        if (calcularIMC() < 16)
             System.out.println("Baixo peso muito grave");
-        else if (p.imc < 17)
+        else if (calcularIMC() < 17)
             System.out.println("Baixo peso grave");
-        else if (p.imc < 18.5)
+        else if (calcularIMC() < 18.5)
             System.out.println("Baixo peso");
-        else if (p.imc < 25.0)
+        else if (calcularIMC() < 25.0)
             System.out.println("Peso normal");
-        else if (p.imc < 30.0)
+        else if (calcularIMC() < 30.0)
             System.out.println("Sobrepeso");
-        else if (p.imc < 35.0)
+        else if (calcularIMC() < 35.0)
             System.out.println("Obesidade grau 1");
-        else if (p.imc < 40.0)
+        else if (calcularIMC() < 40.0)
             System.out.println("Obesidade grau 2");
         else
             System.out.println("Obesidade grau 3: obesidade mórbida");
