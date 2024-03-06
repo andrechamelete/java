55b3c3071bf2331bd8c6bf65c95335373ce56541
andrechamelete <chamelete@gmail.com>
2024-03-06 11:18:56 -0300

    continuando

--- Restaurante/Main.java
+++ Restaurante/Main.java
@@ -1,6 +1,15 @@
-public class Main {
+import java.util.HashMap;
 
+public class Main {
     public static void main(String[]args) {
 
+        Menu xburger = new Menu("X-burger", 25.00);
+
+
+        System.out.println(xburger.nome);
+        System.out.println(xburger.preco);
+        System.out.println(xburger.cardapio);
     }
+
+
 }
--- Restaurante/Menu.java
+++ Restaurante/Menu.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Menu {
-    HashMap<String, Double> cardapio = new HashMap<String, Double>();
 
-    public Menu(String name) {
-    String nome = name;
-    double Preco;
-    ArrayList<String> Ingredientes = new ArrayList<>();
+    static HashMap<String, Double> cardapio = new HashMap<String, Double>();
+    String nome;
+    double preco;
+
+    // class sabe quais pratos (objetos) existem e seus valores:
+    Menu (String name, double precoV) {
+        this.nome = name;
+        this.preco = precoV;
+        ArrayList<String> Ingredientes = new ArrayList<>();
+        cardapio.put(this.nome, this.preco);
+
     }
 }
-
