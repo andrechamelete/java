9e6846316a0de6ab6b8d4fb8802e4c33d4da8b6a
andrechamelete <104088742+andrechamelete@users.noreply.github.com>
2024-03-06 23:37:36 -0300

    Comentários para entendimento

--- IsPrime.java
+++ IsPrime.java
@@ -1,8 +1,12 @@
+//retorna divisores do int inputado ou
+//se este é primo
+
 import java.util.Scanner;
 
 class Prime {
     public static void main(String[]args) {
 
+        //pede input int
         Scanner sc = new Scanner(System.in);
         System.out.print("qual número? ");
 
@@ -11,6 +15,7 @@ class Prime {
 
         boolean flag = false;
 
+        //itera para encontrar divisores de x
         for (int i = 2; i < x; i++) {
             if (x % i == 0) {
                 flag = true;
@@ -19,6 +24,7 @@ class Prime {
             else {
                 continue;}}
 
+        //retorna aviso caso seja primo
         if (!flag) {
             System.out.println(x + " é primo");}
     }
