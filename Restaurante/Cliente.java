3fa7cd62b88a7c7937eff76bcbbe57362174fef3
andrechamelete <chamelete@gmail.com>
2024-03-24 15:52:28 -0300

    criando clientes

--- Restaurante/Cliente.java
+++ Restaurante/Cliente.java
@@ -1,19 +1,33 @@
 package Restaurante;
 
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Cliente{
+    private int id;
     private String nome;
     private String sobrenome;
     private String login;
     private String senha;
     ArrayList<Pedido> compras = new ArrayList<Pedido>();
 
-    public Cliente () {
-        
+    public Cliente(String u, String p) {
+        login = u;
+        senha = p;      
     }
 
-    public void registrar () {
+    public static void registrar () {
+        Scanner reg = new Scanner(System.in);
+        System.out.print("Usuário: ");
+        String user = reg.nextLine();
+        System.out.print("Senha: ");
+        String password = reg.nextLine();
+
+        new Cliente(user, password);
+
+        System.out.println(user);
+        System.out.println(password);
+
 
     }
 
--- Restaurante/Principal.java
+++ Restaurante/Principal.java
@@ -1,7 +1,9 @@
 package Restaurante;
 
 public class Principal {
-    public static void main(String[]args) {     
+    public static void main(String[]args) {   
+        Cliente.registrar();
+        
         Atendimento.Atender();
     }
 }
