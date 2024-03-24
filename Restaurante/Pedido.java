82195f7714fc872e6a604bb98fe63e816e622a51
andrechamelete <chamelete@gmail.com>
2024-03-24 14:28:26 -0300

    novas classes

--- Restaurante/Pedido.java
+++ Restaurante/Pedido.java
@@ -1,40 +1,6 @@
-// classe representa o pagamento do pedido
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Pedido {
-
-    public Pedido() {}
-
-    static void Atendimento() {      
-
-        System.out.println("Olá, bem vindo ao Restaurante! \nAqui está nosso cardápio. Posso anotar seu pedido?");
-        System.out.println("1 - X-Burger");
-        System.out.println("2 - X-Bacon");
-        System.out.println("3 - X-Salada");
-        
-        Pedir();        
-    }
-
-    static void Pedir() {
-        System.out.print("O que você deseja? (digite o número): ");
-        Scanner opt = new Scanner(System.in);
-        int input = opt.nextInt();
-
-        switch (input) {
-            case 1:
-                System.out.println("Pedido: 1 X-Burger!");
-                break;
-            case 2:
-                System.out.println("Pedido: 1 X-Bacon!");
-                break;
-            case 3:
-                System.out.println("Pedido: 1 X-Salada!");
-                break;
-            default:
-                System.out.println("Não é um pedido válido");
-                Pedir();
-                 
-        }
-        opt.close();    
-    }
+    private ArrayList<String> itens = new ArrayList<String>();
+    private Double Total;
 }
