754cdb5dac968ac9dc62c48b680631456dd47309
andrechamelete <104088742+andrechamelete@users.noreply.github.com>
2024-03-24 14:31:23 -0300

    Cliente.java

---
+++ Restaurante/Cliente.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Cliente{
+    private String nome;
+    private String sobrenome;
+    private String login;
+    private String senha;
+    ArrayList<Pedido> compras = new ArrayList<Pedido>();
+
+}
