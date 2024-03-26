e5a7b043bc5fdbc9c170387074b0bba3d4a9390c
andrechamelete <chamelete@gmail.com>
2024-03-26 12:49:06 -0300

    Banco

--- Banco/Main.java
+++ Banco/Main.java
@@ -2,7 +2,7 @@ package Banco;
 
 class Main {
     public static void main(String[]args) {
-        ContaCorrente chame = new ContaCorrente("Andre", "BA0001");
+        ContaCorrente chame = new ContaCorrente("Andre", "CC0001");
         chame.showMenu();
     }
 }
